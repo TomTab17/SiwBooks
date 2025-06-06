5fc9d1a65c277e174d61e1fd11cf3e71e6969d5d
Tommaso Tabacchini <tom.tabacchini@stud.uniroma3.it>
2025-06-06 12:00:44 +0200

    initial project files

---
+++ src/main/java/it/uniroma3/siw/SiwBooks/SiwBooksApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.SiwBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwBooksApplication.class, args);
+	}
+
+}
